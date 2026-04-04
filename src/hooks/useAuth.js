fe665207bac174cd47b3078309138894ae4321c8
Sofia <Sofia>
2026-04-04 15:11:55 -0500

    chore(all): Change firebase.js to firebase.ts with security improvements

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,7 +1,7 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 
-import { auth } from '@/firebase';
+import { auth } from '@/firebase.ts';
 
 export function useAuth() {
   const [user, setUser] = useState(null);

ea082f14cb84ce230e10b89339534ce50d036945
BoneBiter255 <Felix2295772@gmail.com>
2026-04-05 13:13:33 -0500

    feat(chat-videocall) integracion de artefacto de zego intento 1

--- src/features/chat-videocall/index.ts
+++ src/features/chat-videocall/index.ts
@@ -1,2 +1,3 @@
 export { ChatProvider, useChatContext } from './components/ChatContext.tsx';
 export { default as VideoCallPage } from './VideoCallPage.jsx';
+export { ZEGO_CONFIG } from './components/zego-config.js';

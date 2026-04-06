5da90f01b986cbef93ebf178cf25d63fcfca2f1e
Sofia <Sofia>
2026-04-05 19:42:12 -0500

    refactor(reports): rename components and update imports for reports module. Improve reports feature

--- src/features/board/hooks/index.ts
+++ src/features/board/hooks/index.ts
@@ -2,5 +2,6 @@ export * from '../../reports/hooks/useTasksApi.ts';
 export * from './useCreateTask';
 export * from './useDeleteTask';
 export * from './useTasks';
+export * from '../../reports/hooks/useTasksApi.ts';
 export * from './useUpdateTask';
 export * from './useValidTaskIds';

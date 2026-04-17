62a0357f7c6f89e46166ba9528e18278af0e038b
Marlio <marlio.charry-e@mail.escuelaing.edu.co>
2026-04-17 11:25:11 -0500

    feat(Sound): New sounds for effects

--- src/hooks/useSoundEffects.js
+++ src/hooks/useSoundEffects.js
@@ -9,24 +9,25 @@ export const useSoundEffects = () => {
     taskCompleted: '/sounds/task-completed.mp3',
     taskUpdated: '/sounds/task-updated.mp3',
     taskDeleted: '/sounds/task-deleted.mp3',
-    
+
+
     // Efectos de UI
     click: '/sounds/click.mp3',
     hover: '/sounds/hover.mp3',
     success: '/sounds/success.mp3',
     error: '/sounds/error.mp3',
     notification: '/sounds/notification.mp3',
-    
+
     // Efectos de arrastrar y soltar
     dragStart: '/sounds/drag-start.mp3',
     dragEnd: '/sounds/drag-end.mp3',
     drop: '/sounds/drop.mp3',
-    
+
     // Efectos de navegación
     pageLoad: '/sounds/page-load.mp3',
     modalOpen: '/sounds/modal-open.mp3',
     modalClose: '/sounds/modal-close.mp3',
-    
+
     // Efectos de chat
     messageSent: '/sounds/message-sent.mp3',
     messageReceived: '/sounds/message-received.mp3',

0bc860347761e0ecc1986921c88462081492fccf
Marlio <marlio.charry-e@mail.escuelaing.edu.co>
2026-04-17 12:26:09 -0500

    feat(Sound): New Approach for navbar sounds

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import './app.css';
 
 import Router from './Router';
 import { AudioProvider } from '@/context/AudioContext';
-import AudioNavbar from '@/components/AudioNavbar';
 
 export default function App() {
   return (
     <AudioProvider>
-      <AudioNavbar />
       <Router />
     </AudioProvider>
   );
